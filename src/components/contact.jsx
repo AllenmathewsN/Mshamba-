a4f4b36582554047f8295ee01075db1b2cc80e01
Allen <allenmathews416@gmail.com>
2022-10-14 01:12:52 +0300

    contact

---
+++ src/components/contact.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Contacts() {
+    return (
+        <div className="contact-right">
+        <h3>Reach Us</h3>
+
+        <table>
+            <tr>
+                <td>Email:</td>
+                <td>mshambakenya@gmail.com      orders@mshambakenya.com</td>
+            </tr>
+            <tr>
+                <td>Phone:</td>
+                <td>+254 7456 789   +254 1024 998</td>
+            </tr>
+            <tr>
+                <td>Address:</td>
+                <td>Ngong Lane, Ngong Lane Plaza,   1st Floor,Nairobi Kenya
+                </td>
+            </tr>
+        </table>
+</div>
+    )
+}
+
+export default Contacts;
